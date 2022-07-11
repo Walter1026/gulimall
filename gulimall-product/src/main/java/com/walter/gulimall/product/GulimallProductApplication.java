124f2d3d6a379b34c59d61010dc5768b0de19920
walter <kdwalter@163.com>
2022-07-12 00:34:51 +0800

    基本CRUD功能

--- gulimall-product/src/main/java/com/walter/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/walter/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,10 @@
 package com.walter.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.walter.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
--- gulimall-product/src/test/java/com/walter/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/walter/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,26 @@
 package com.walter.gulimall.product;
 
+import com.walter.gulimall.product.entity.BrandEntity;
+import com.walter.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("华为");
+        brandService.save(brandEntity);
+        System.out.println("保存成功。。。。");
     }
 
 }
